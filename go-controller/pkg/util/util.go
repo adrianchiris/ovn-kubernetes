2e0b63d472d2b26aba6479ccfd5905a762a615f9
Adrian Chiris <adrianc@nvidia.com>
2021-02-14 15:38:49 +0000

    Add utility function: RunPeriodicallyUntilStop
    Executes a callable in a periodic manner to allow code reuse
    by ovnkube node to call healthcheck and monitoring methods:
     - node.checkForStaleOVSInterfaces()
     - ManagementPort.CheckManagementPortHealth()
    
    Subsequent commit will take into account this change
    and call the mentioned methods via RunPeriodicallyUntilStop(fn)
    
    Signed-off-by: Adrian Chiris <adrianc@nvidia.com>

--- go-controller/pkg/util/util.go
+++ go-controller/pkg/util/util.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
@@ -186,5 +187,16 @@ func HashForOVN(s string) string {
 	}
 	hashString := strconv.FormatUint(h.Sum64(), 10)
 	return fmt.Sprintf("a%s", hashString)
+}
 
+// RunPeriodicallyUntilStop calls callable every period time until stopChan is posted an object
+func RunPeriodicallyUntilStop(callable func(), period time.Duration, stopChan chan struct{}) {
+	for {
+		select {
+		case <-time.After(period):
+			callable()
+		case <-stopChan:
+			return
+		}
+	}
 }
